9522e04eae821a53253a7c3f4f03a94f4b5ffdee
Ashwinee Kumar Dhakad <ashwinee.dhakde@gmail.com>
2025-12-27 00:06:17 +0530

    Initial Commit

---
+++ DualCamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DualCamera
+//
+//  Created by Ashwinee Dhakde on 27/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DualCamera/DualCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualCameraApp.swift
+//  DualCamera
+//
+//  Created by Ashwinee Dhakde on 27/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DualCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
